e5b52d92c6350d193cd79674c9767827a00ab428
alex.fan <alex.fan@okchem.com>
2019-09-11 11:08:55 +0800

    feature: event delegation pattern

--- src/app/main.js
+++ src/app/main.js
@@ -1,1 +1,27 @@
-console.log("hello world!");
+class Menu {
+  constructor(elem) {
+    this._elem = elem;
+    elem.onclick = this.onClick.bind(this); // (*)
+  }
+
+  save() {
+    alert("saving");
+  }
+
+  load() {
+    alert("loading");
+  }
+
+  search() {
+    alert("searching");
+  }
+
+  onClick(event) {
+    let action = event.target.dataset.action;
+    if (action) {
+      this[action]();
+    }
+  }
+}
+
+new Menu(menu);
